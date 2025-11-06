8e335b62af73f01f7cfd51b7601e77d54b2699f6
Muhammad Daud <mdaud9062@gmail.com>
2025-11-06 19:36:16 +0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
-// export const API_BASE_URL = "http://mern-env.eba-qyah2eg7.ap-south-1.elasticbeanstalk.com";
-// export const API_BASE_URL = "http://localhost:3000";
+// export const API_BASE_URL = """;
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
